c45c7c3d8eb6f841a7a1bde1e4154a2332ea4005
stijn577 <stijnschotte03@gmail.com>
2024-07-02 17:16:56 +0200

    02/07/2024

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@
 #![no_main]
 
 use defmt_rtt as _;
-use embassy_time::Instant;
+use embassy_time::{Instant, Timer};
 use panic_probe as _;
 
 use defmt::info;
@@ -10,15 +10,39 @@ use embassy_executor::Spawner;
 use embassy_stm32::{
     exti::ExtiInput,
     gpio::{Input, Level, Output, Pull, Speed},
+    peripherals::{PC13, PC7},
 };
 
+
+/// A macro to benchmark the execution time of a function.
+///
+/// # Parameters
+///
+/// * `$fn`: The function to be benchmarked.
+/// * `$freq`: The expected frequency of the function in Hz.
+///
+/// # Returns
+///
+/// This macro does not return any value. It logs the execution time of the function in seconds and microseconds.
+///
+/// # Example
+///
+/// ```rust
+/// qbench!(led.set_high(), 4_000_000);
+/// ```
+/// this would then output: something like
+/// ```bash
+/// [ 140.236815 INFO src/main.rs:26  ] "led.set_high()" took 7.25e-6s = 7.25us
+/// ```
 macro_rules! qbench {
     ($fn: expr, $freq: literal) => {{
         let before = Instant::now();
         $fn;
-        let diff = Instant::now() - before;
-        diff.as_ticks() / ($freq * 1_000_000)
-
+        let after = Instant::now();
+        let diff = after - before;
+        let s = diff.as_ticks() as f64 / $freq as f64;
+        let us = s * 1_000_000.0;
+        info!("{:?} took {:?}s = {:?}us", stringify!($fn), s, us)
     }};
 }
 
@@ -28,21 +52,33 @@ async fn main(_s: Spawner) {
 
     info!("Hello world");
 
-    let mut led = Output::new(pp.PC7, Level::Low, Speed::Low);
+    let led = Output::new(pp.PC7, Level::Low, Speed::Low);
     // Warning:
     // The PC13 I/O used for the user button must be set to INPUT, pull‑down (PD) with
     // debouncing. Never set the PC13 to OUTPUT/LOW level to avoid a shortcut when the user
     // button is pressed.
-    let mut btn = ExtiInput::new(Input::new(pp.PC13, Pull::Down), pp.EXTI13);
+    let btn = ExtiInput::new(Input::new(pp.PC13, Pull::Down), pp.EXTI13);
 
+    _s.spawn(blinky(btn, led)).ok();
+}
+
+#[embassy_executor::task]
+async fn blinky(mut btn: ExtiInput<'static, PC13>, mut led: Output<'static, PC7>) {
     loop {
-        info!("waiting for rising edge...");
-        btn.wait_for_rising_edge().await;
-        
-        info!("{:?}us", qbench!(led.set_high(), 4_000_000)); // scale ticks to microsecondes (see ticks_hz_x used for embassy_time dependency)
+        qbench!(led.set_high(), 4_000_000);
 
-        info!("waiting for falling edge...");
-        btn.wait_for_falling_edge().await;
-        led.set_low();
+        Timer::after_secs(1).await;
+
+        qbench!(led.set_low(), 4_000_000);
+
+        Timer::after_secs(1).await;
+
+        qbench!(led.toggle(), 4_000_000);
+
+        Timer::after_secs(1).await;
+
+        qbench!(led.toggle(), 4_000_000);
+
+        Timer::after_secs(1).await;
     }
 }
